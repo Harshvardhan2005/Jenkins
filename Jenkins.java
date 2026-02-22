fd36dd2764542f7940e36a40d654170c62ccccab
Harshvardhan Borude <harshborude05@gmail.com>
2026-02-22 13:25:14 +0530

    Update Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -1,5 +1,7 @@
 public class main { 
     public static void main(String[] args) { 
-        System.out.println("Hello from Jenkins Build!"); 
+        System.out.println("Hello from Jenkins Build!");
+        System.out.println("Webhook Trigger Test");
     } 
+
 }
